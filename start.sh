50f6b3c608a5a625d1507ac03a44bec69bca38e5
victorhearnyeates <victoryeates@googlemail.com>
2018-11-29 23:12:59 +0000

    Upgraded elasticsearch to 6.3.1

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./bin/elasticsearch -Epath.conf=config_node_one &
-./bin/elasticsearch -Epath.conf=config_node_two &
-./bin/elasticsearch -Epath.conf=config_node_three &
+ES_PATH_CONF=config_node_one ./bin/elasticsearch &
+ES_PATH_CONF=config_node_two ./bin/elasticsearch &
+ES_PATH_CONF=config_node_three ./bin/elasticsearch &
--- stop.sh
+++ stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #MacOS
-#kill $(ps | grep elasticsearch | cut -d' ' -f1 | tr '\n' ' ')
+kill $(ps | grep elasticsearch | cut -d' ' -f1 | tr '\n' ' ')
 
 #Linux
-kill $(ps aux | grep elasticsearch | cut -d' ' -f4 | tr '\n' ' ')
+#kill $(ps aux | grep elasticsearch | cut -d' ' -f4 | tr '\n' ' ')
 
